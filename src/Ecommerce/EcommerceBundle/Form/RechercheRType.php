f53ab562677e1a42f8d83292cc93842c20c5bb93
fatimaDIANI <diani.fatimaz@gmail.com>
2017-05-04 22:59:10 +0200

    Update RechercheRType.php

--- src/Ecommerce/EcommerceBundle/Form/RechercheRType.php
+++ src/Ecommerce/EcommerceBundle/Form/RechercheRType.php
@@ -5,13 +5,11 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RechercheRType extends AbstractType
-{
-    public function buildForm(FormbuilderInterface $builder, array $option)
+{   public function buildForm(FormbuilderInterface $builder, array $option)
     {
         $builder->add('recherche','text', array('label' => false,
                                                 'attr' => array('class' => 'input-medium search-query')));
     }
-    
     public function getName()
     {
         return 'ecommerce_ecommercebundle_recherche';
